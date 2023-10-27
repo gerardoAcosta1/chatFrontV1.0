59bc38dd2c5883a6a71f700b0ba141b74042c3b7
GerardoAcosta1 <gerardonuevo1234321@gmail.com>
2023-10-26 22:27:43 -0600

    se elminó el puerto url directa socket sin puerto

--- src/utils/initWebSocket.jsx
+++ src/utils/initWebSocket.jsx
@@ -2,7 +2,7 @@ import io  from "socket.io-client";
 let socket = null;
 export const initWebSocket = () => {
     if (!socket) {
-        socket = io('https://chatv1-0.onrender.com:8000/');
+        socket = io('https://chatv1-0.onrender.com/');
     
         socket.on('connect', () => {
           console.log('Conexión establecida con el servidor WebSocket');
